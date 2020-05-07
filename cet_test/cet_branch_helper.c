0da9e73a49f69001fc6643a1610ec9e1bef7af10
shravanrn@gmail.com <shravanrn@gmail.com>
2020-05-06 20:24:56 -0700

    Slightly better signal handling in cet tests

--- cet_test/cet_branch_helper.c
+++ cet_test/cet_branch_helper.c
@@ -18,9 +18,18 @@ typedef int(*FuncType)();
 
 int beginTest()
 {
-    signal(SIGILL, sig_handler);
-    signal(SIGABRT, sig_handler);
-    signal(SIGSEGV, sig_handler);
+    if (signal(SIGILL, sig_handler) == SIG_ERR) {
+        printf("Setting signal handler for SIGILL failed\n");
+        exit(1);
+    }
+    if (signal(SIGABRT, sig_handler) == SIG_ERR) {
+        printf("Setting signal handler for SIGABRT failed\n");
+        exit(1);
+    }
+    if (signal(SIGSEGV, sig_handler) == SIG_ERR) {
+        printf("Setting signal handler for SIGSEGV failed\n");
+        exit(1);
+    }
 
     char* func_ptr = (char*) indirect_call;
     // When compiled with CET branch checking indirect_call starts with the 4 byte endbr instruction
@@ -34,6 +43,10 @@ int beginTest()
         printf("CET App Failed\n");
         exit(1);
     }
+
+    signal(SIGILL, SIG_DFL);
+    signal(SIGABRT, SIG_DFL);
+    signal(SIGSEGV, SIG_DFL);
 
     return 0;
 }
