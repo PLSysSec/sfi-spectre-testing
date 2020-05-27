887697462f15e5ff219922f6d1c27bb1abb5c8ce
Evan Johnson <e5johnso>
2020-05-27 13:41:55 -0700

    apparently I had some uncommited script changes

--- scripts/spec_stats.py
+++ scripts/spec_stats.py
@@ -2,7 +2,8 @@ import sys
 from collections import defaultdict
 import matplotlib.pyplot as plt
 import numpy as np
-
+import argparse
+from matplotlib.ticker import FuncFormatter
 
 #prefix = "sfi-spectre-spec/result/"
 
@@ -62,7 +63,7 @@ def all_times_to_vals(all_times):
     return vals
 
 
-def make_graph(all_times, output_path):
+def make_graph(all_times, output_path, use_percent=False):
     fig = plt.figure()
     num_mitigations = len(all_times)
     num_benches = len(next(iter(all_times.values()))) # get any element
@@ -88,6 +89,12 @@ def make_graph(all_times, output_path):
     ax.set_xticks(ind+width)
     plt.xticks(rotation=90)
 
+    plt.axhline(y=1.0, color='black', linestyle='dashed')
+    plt.ylim(ymin=.8)
+
+    if use_percent:
+        ax.yaxis.set_major_formatter(FuncFormatter(lambda y, _: '{:.0%}'.format(y-1.0)))
+
     ax.set_xticklabels(labels)
     ax.legend( tuple(rects), all_times.keys() )
     fig.subplots_adjust(bottom=0.25)
@@ -97,10 +104,11 @@ def make_graph(all_times, output_path):
         result_average = sum(vals[i]) / num_benches
         result_median = median(vals[i])
         #print(f"{mitigations[i]} average = {result_average} {mitigations[i]} median = {result_median}")
-        with open(output_path + "/stats", "a") as myfile:
+        with open(output_path + ".stats", "a") as myfile:
             myfile.write(f"{mitigations[i]} average = {result_average} {mitigations[i]} median = {result_median}\n")
 
-    plt.savefig(output_path + "/graph", format="pdf")
+    plt.tight_layout()
+    plt.savefig(output_path + ".graph", format="pdf")
     '''
     for i in range(num_mitigations):
         result_average = sum(vals[i]) / N 
@@ -111,7 +119,7 @@ def make_graph(all_times, output_path):
     plt.savefig(outfile, format="pdf")
     '''
 
-    plt.show()
+    #plt.show()
 
 def get_merged_summary(result_path, n):
     int_input_path = f"{result_path}/CINT2006.{str(n).zfill(3)}.ref.rsf"
@@ -146,16 +154,86 @@ def run(result_path, n, output_path):
    
     normalized_times = normalize_times(all_times)
    
-    #{mitigation name -> {}}
+    #{mitigation name -> {}}      --- here is where we cut
     make_graph(normalized_times, output_path)
   
 
+def run_w_filter(result_path, bench_filter, n, use_percent):
+    lock_num = int(get_lock_num(result_path))
+    all_times = {}
+    for idx in range(n):
+        name,times = get_merged_summary(result_path, lock_num - n + idx + 1)
+        print(name, times)
+        all_times[name] = times
+
+    normalized_times = normalize_times(all_times)
+
+    #{mitigation name -> {}}      --- here is where we cut
+    for partitioned_times, output_path in bench_filter.partition_benches(normalized_times):
+        make_graph(partitioned_times, output_path,  use_percent=use_percent)
+
+
+class BenchAlias(object):
+        """docstring for BenchAlias"""
+        def __init__(self, arg):
+            name,aliased_as = arg.split(":")
+            self.name = name
+            self.aliased_as = aliased_as
+
+        def __repr__(self):
+            return self.__str__()
+
+        def __str__(self):
+            return f"{self.name} -> {self.aliased_as}"
+
+def parse_bench_filter(s):
+    d = {}
+    benchsets = s.split(";")
+    for benchset in benchsets:
+        out_path,alias_list = benchset.split("=")
+        parsed_aliases = [BenchAlias(alias) for alias in alias_list.split(",")]
+        d[out_path] = parsed_aliases
+
+    return d
+
+class BenchFilter(object):
+        """docstring for BenchFilter"""
+        def __init__(self, s):
+                self.filter = parse_bench_filter(s)
+
+        def get_total_mitigation_num(self):
+            n = 0
+            for bencheset in self.filter.values():
+                n += len(bencheset)
+            return n
+
+        # normalized times = {mitigation_name -> {bench_name -> time}}
+        # bench_aliases = [(name, alias)]
+        # result = {mitigation_name (aliased) -> {bench_name -> time}}
+        def partition_one(self, normalized_times, bench_aliases):
+            d = {}
+            for alias in bench_aliases:
+                times = normalized_times[alias.name]
+                d[alias.aliased_as] = times
+            return d
+
+        def partition_benches(self, normalized_times):
+            for output_path, bench_aliases in self.filter.items():
+                partitioned_benches = self.partition_one(normalized_times, bench_aliases)
+                yield partitioned_benches, output_path
+
+        def __str__(self):
+            return str(self.filter)
 
 def main():
-    if len(sys.argv) != 4:
-        print("Usage: python spec_stats.py <path to sfi spec file> <# of mitigations> <output path>")
-        sys.exit()
-    run(sys.argv[1], int(sys.argv[2]), sys.argv[3])
+    parser = argparse.ArgumentParser(description='Graph Spec Results')
+    parser.add_argument('-i', dest='input_path', help='input directory (should be a spec2017 results directory)')
+    parser.add_argument('--usePercent', dest='usePercent', default=False, action='store_true')
+    parser.add_argument("--filter", dest="filter", type=BenchFilter)
+    parser.add_argument("-n", dest="n", type=int)
+    args = parser.parse_args()
+    run_w_filter(args.input_path, args.filter, args.n, use_percent=args.usePercent)
+
 
 if __name__ == '__main__':
     main()
